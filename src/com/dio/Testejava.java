1341624896fbb92d0b8f7ae6f8826990f6a237ed
olga <olgabeatrizs@icloud.com>
2022-07-16 18:45:44 -0300

    Criação de calculadora

---
+++ src/com/dio/Testejava.java
@@ -0,0 +1,13 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class Testejava {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		System.out.println(gato);
+	}
+
+}
---
+++ src/com/dio/model/Gato.java
@@ -0,0 +1,51 @@
+package com.dio.model;
+
+public class Gato {
+
+	private String nome;
+	private String cor;
+	private Integer idade;
+	/**
+	 * @return the nome
+	 */
+	public String getNome() {
+		return nome;
+	}
+	/**
+	 * @param nome the nome to set
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	/**
+	 * @return the cor
+	 */
+	public String getCor() {
+		return cor;
+	}
+	/**
+	 * @param cor the cor to set
+	 */
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	/**
+	 * @return the idade
+	 */
+	public Integer getIdade() {
+		return idade;
+	}
+	/**
+	 * @param idade the idade to set
+	 */
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+	
+	
+	
+	
+	
+	
+	
+}
